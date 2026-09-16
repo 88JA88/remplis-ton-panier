e236c9fd728cd01019da26ebf37fd0c80d9fabff
88JA88 <j.artigues@icloud.com>
2026-09-16 16:00:30 +0200

    Ouvrir le selecteur de restauration sur iPhone

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'liste-courses-v24';
+const CACHE = 'liste-courses-v25';
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['./', './index.html', './style.css', './app.js', './manifest.webmanifest', './icon.svg']))));
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
