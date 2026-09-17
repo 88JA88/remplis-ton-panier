78e00d9991701417591511498b3111775e455240
88JA88 <j.artigues@icloud.com>
2026-09-17 10:01:02 +0200

    Actualiser le cache de l application

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,12 @@
-const CACHE = 'liste-courses-v27';
+const CACHE = 'liste-courses-v28';
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['./', './index.html', './style.css', './app.js', './manifest.webmanifest', './icon.svg']))));
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
-self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => { const copy = response.clone(); caches.open(CACHE).then(cache => cache.put(event.request, copy)); return response; }).catch(() => caches.match('./index.html')))));
+self.addEventListener('fetch', event => event.respondWith(
+  fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request).then(cached => cached || caches.match('./index.html')))
+));
